8f7133ccad770aebb6fbbb5103ef9c2ddc967fa4
alina <mlkysheikh@gmail.com>
2023-02-19 18:13:23 -0500

    edit playlist

--- src/components/UserPlaylistResults/index.js
+++ src/components/UserPlaylistResults/index.js
@@ -1,3 +1,5 @@
+import { Pencil } from "phosphor-react";
+
 const UserPlaylistResults = ({ playlist }) => {
   const cover = playlist.images[0]?.url;
   const title = playlist.name;
@@ -11,6 +13,12 @@ const UserPlaylistResults = ({ playlist }) => {
           <div className="font-bold text-main text-sm py-1">{title}</div>
           <div className="text-main text-xs font-medium">{description}</div>
         </div>
+      </div>
+      <div className="flex flex-row gap-3">
+        <Pencil
+          size="1.5rem"
+          className="text-main hover:text-primary cursor-pointer"
+        />
       </div>
     </div>
   );
