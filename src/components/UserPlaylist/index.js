351ec730d6d8add5d322e7dd7c7a8bb39e95140d
alina <mlkysheikh@gmail.com>
2023-02-19 17:16:18 -0500

    stuff

--- src/components/UserPlaylist/index.js
+++ src/components/UserPlaylist/index.js
@@ -1,19 +1,28 @@
 import React, { useState, useEffect } from "react";
 import getUsersPlaylists from "../../api/getUsersPlaylists";
 import useUser from "../../hooks/useUser";
+import UserPlaylistResults from "../UserPlaylistResults";
 
 const UserPlaylist = () => {
   const { userId, token } = useUser();
+  const [playlists, setPlaylists] = useState([]);
 
   const getPlaylists = async () => {
     const data = await getUsersPlaylists(token);
     const playlists = data.items.filter((item) => item.owner.id === userId);
-    console.log(playlists);
+    setPlaylists(playlists);
   };
 
   return (
     <div>
       <button onClick={getPlaylists}>Click me!</button>
+      {playlists.length > 0 && (
+        <div className="playlist-dropdown">
+          {playlists.map((playlist) => (
+            <UserPlaylistResults key={playlist.id} playlist={playlist} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
