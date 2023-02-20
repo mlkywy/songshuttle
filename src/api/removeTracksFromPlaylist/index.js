62d4ee4c88fbf5f933811e3282dec7cac1defc72
alina <mlkysheikh@gmail.com>
2023-02-19 23:57:34 -0500

    we're balling

--- src/api/removeTracksFromPlaylist/index.js
+++ src/api/removeTracksFromPlaylist/index.js
@@ -1,14 +1,34 @@
 import axios from "axios";
 import { SPOTIFY_ENDPOINTS } from "../constants";
 import convertEndpointParams from "../../utils/convertEndpointParams";
+import getPlaylistTracks from "../getPlaylistTracks";
 
-const removeTracksFromPlaylist = async (auth, playlistId, songIds) => {
+const removeTracksFromPlaylist = async (auth, playlistId, uris) => {
   const headers = { Authorization: `Bearer ${auth}` };
   const params = { playlistId: playlistId };
-
-  console.log(auth, playlistId, songIds);
+  const tracks = [];
 
   try {
+    // Get the position of each track within the playlist
+    const allTracks = await getPlaylistTracks(auth, playlistId);
+
+    // Find the position of each track within the playlist
+    allTracks.forEach((item, index) => {
+      const uri = item.uri;
+      if (uris.includes(uri)) {
+        let trackToDelete = {
+          uri: uri,
+          positions: [index],
+        };
+
+        // If this track has not already been added to the tracks array, add it
+        if (!tracks.some((track) => track.uri === uri)) {
+          tracks.push(trackToDelete);
+        }
+      }
+    });
+
+    // Send the request to remove the tracks by their position
     const { data } = await axios.delete(
       convertEndpointParams(
         SPOTIFY_ENDPOINTS.REMOVE_TRACKS_FROM_PLAYLIST,
@@ -16,7 +36,7 @@ const removeTracksFromPlaylist = async (auth, playlistId, songIds) => {
       ),
       {
         headers,
-        data: { uris: songIds },
+        data: { tracks },
       }
     );
     return data;
