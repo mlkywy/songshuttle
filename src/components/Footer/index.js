c17a6cc4e0bc156e40643b9f9956f86ec013a40c
alina <mlkysheikh@gmail.com>
2023-02-21 13:13:12 -0500

    Update footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,30 +4,9 @@ import { GithubLogo, LinkedinLogo, TwitterLogo } from "phosphor-react";
 const Footer = () => {
   return (
     <footer>
-      <div className="fixed bottom-0 bg-primary w-full flex flex-row items-center justify-start gap-20 h-20 md:px-48 lg:px-64">
-        <div className="flex flex-row items-center justify-between gap-5 text-main font-medium">
+      <div className="fixed bottom-0 bg-primary w-full flex flex-row items-center justify-center gap-20 h-20 md:px-48 lg:px-64">
+        <div className="flex flex-row items-center text-main font-medium">
           <p className="text-xs">Made with ♥ by Alina!</p>
-          <a href="https://github.com/mlkywy" target="_blank" rel="noreferrer">
-            <div>
-              <GithubLogo size="1.5rem" />
-            </div>
-          </a>
-
-          <a
-            href="https://www.linkedin.com/in/alina-sheikh-a59a4518a/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <div>
-              <LinkedinLogo size="1.5rem" />
-            </div>
-          </a>
-
-          <a href="https://twitter.com/wymlky" target="_blank" rel="noreferrer">
-            <div>
-              <TwitterLogo size="1.5rem" />
-            </div>
-          </a>
         </div>
 
         <div className="flex flex-row items-center justify-between gap-5 font-medium">
